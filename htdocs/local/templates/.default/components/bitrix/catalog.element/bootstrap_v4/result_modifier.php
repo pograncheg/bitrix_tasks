836bd349453e2f7923880f0d72a505df0faa7eb5
pogran <pograncheg@gmail.com>
2024-09-21 18:50:41 +0300

    Задача №6 Битрикс

---
+++ htdocs/local/templates/.default/components/bitrix/catalog.element/bootstrap_v4/result_modifier.php
@@ -0,0 +1,11 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var CBitrixComponentTemplate $this
+ * @var CatalogElementComponent $component
+ */
+
+$component = $this->getComponent();
+$arParams = $component->applyTemplateModifications();
+// echo '<pre>'; print_r($arParams); echo '</pre>';
+// echo '<pre>'; print_r($arResult); echo '</pre>';
